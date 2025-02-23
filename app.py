4b5a1d5a5e1cace319f0014277dc2a55a70c47c6
gonlyplease <gerhi13@gmail.com>
2025-02-23 12:10:27 +0100

    Fix sqalchemy error

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# app.py
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
@@ -11,9 +12,13 @@ import pickle
 from dotenv import (
     load_dotenv,
 )  # use python-dotenv in your Python script to load the environment variables
-import sqlalchemy
 from models import db, RevenueData
 from revenue_extractor import create_gemini_client, extract_revenue_from_pdf
+
+import cryptography
+
+print("Cryptography version:", cryptography.__version__)
+
 
 # load the environment variables from .env file
 load_dotenv()
@@ -35,17 +40,13 @@ os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 
 # configure loggin to catch capture errors and infos
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# gemini implementation
-"""response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents="Whats the revenue in 2022"
-)"""
 
 # Create a Gemini API client (make sure GEMINI_API_KEY is set in your .env)
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
@@ -53,13 +54,13 @@ gemini_client = genai.Client(api_key=GEMINI_KEY)
 MODEL_ID = "gemini-2.0-flash-001"
 
 
-# vector Store Setup
+"""# vector Store Setup
 vector_store = None
 
 # Load existing vector store if available
 if os.path.exists("vector_store.pkl"):
     with open("vector_store.pkl", "rb") as f:
-        vector_store = pickle.load(f)
+        vector_store = pickle.load(f)"""
 
 
 @app.errorhandler(404)
@@ -85,20 +86,20 @@ def upload():
             flash("No file selected")
             return redirect(url_for("index"))
 
-        # Save the uploaded file
+        # save the uploaded file
         file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
         file.save(file_path)
 
-        # Extract revenue data from the PDF
+        # extract the json revenue data from the PDF
         revenue_extraction = extract_revenue_from_pdf(
             file_path, gemini_client, MODEL_ID
         )
 
-        # Insert the extracted data into the database
+        # insert data into the database
         new_entry = RevenueData(
             company_name=revenue_extraction.company_name,
             year=revenue_extraction.year,
-            revenue=int(revenue_extraction.revenue),  # Convert to integer if necessary
+            revenue=int(revenue_extraction.revenue),  # convert to integer if necessary
             currency=revenue_extraction.currency,
         )
         db.session.add(new_entry)
